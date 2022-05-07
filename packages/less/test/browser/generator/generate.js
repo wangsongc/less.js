890a4c708d07dc606943f2b1c843b4914ff00e89
wangsongc <wangsongc@foxmail.com>
2022-05-07 15:37:56 +0800

    test: timeout 12000

--- packages/less/test/browser/generator/generate.js
+++ packages/less/test/browser/generator/generate.js
@@ -56,7 +56,7 @@ Object.entries(config).forEach(entry => {
         console.log(file)
         return runner({
             file,
-            timeout: 0,
+            timeout: 120000,
             polling: 'raf', 
             args: ['disable-web-security']
         })
